d3fe68724104429a7230cfeb5d22db456bc62920
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2025-04-26 19:16:17 +0000

    up

--- src/learn-2/e.ts
+++ src/learn-2/e.ts
@@ -1,7 +1,7 @@
 // Тип по строковому шаблону
 // Опишите тип, который бы соответствовал шаблону ${любое целое число}${px или %}.
 
-type ValueOfWidth = `${number}${"px" | "%"}`;
+type ValueOfWidth = `${bigint}${"px" | "%"}`;
 
 const px: ValueOfWidth = "100px";
 const procent: ValueOfWidth = "50%";
